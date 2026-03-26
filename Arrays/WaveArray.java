22db803152e8498b47c5fb5fffbea682cd4dc15f
Animesh <unknowndevilcoder@gmail.com>
2026-03-26 14:29:16 +0530

    Arrays in  JAVA commit

--- Arrays/WaveArray.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public static void convertToWave(int[] arr) {
-        for(int i=1;i<arr.length;i+=2){
-            int temp = arr[i];
-            arr[i] = arr[i-1];
-            arr[i-1] = temp;
-        }
-    }
-}
