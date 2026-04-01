1f99b8b69214de544aae25f0f39c147a3be1b1cb
Animesh <unknowndevilcoder@gmail.com>
2026-04-01 23:46:32 +0530

    If-else in JAVA

--- SortingT/kthSmallest.java
+++ SortingT/kthSmallest.java
@@ -1,7 +1,6 @@
 class Solution {
     public static int kthSmallest(int[] arr, int k) {
         int n = arr.length;
-        // TC = O(n*k)
         for(int i=0;i<k;i++) { // n-1 passes
             int min = Integer.MAX_VALUE, mindx = -1;
             for(int j=i;j<n;j++) {
